b9b31fcf2ce4eebe1c64142ce18ae8b792720869
DragonSlayerXavier <amritmahendrajoseph@gmail.com>
2023-06-25 22:59:08 +0530

    Documentation

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var data; // The JSON file
 var MAX_LIVES; // The maximum number of lives
 var lives; // The current number of lives
-var rand = -1;
-var picked = [];
-var killed = [];
-var displayHistory = false;
-var futile = 0;
-var found = 0;
+var rand = -1; // The index of the current question
+var picked = []; // The array of questions that have already been picked
+var killed = []; // The array of functions that have already been killed
+var displayHistory = false; // Whether or not the history is being displayed
+var futile = 0; // The number of times the user has hit check without killing a function.
+var found = 0; // The number of buggy functions the user has found.
+
+/**
+ * Parses markdown text into HTML.
+ * @param {String} text The markdown text to be parsed.
+ * @returns {String} The HTML version of the markdown text.
+ */
 const markdownParser = (text) => {
     const toHTML = text
         .replace(/^\s*\n-/gm, '<ul>\n-')
@@ -24,6 +30,9 @@ const markdownParser = (text) => {
     return toHTML.trim(); // using trim method to remove whitespace
 }
 
+/**
+ * Shows or hides the input history.
+ */
 function toggleHistory() {
     if (displayHistory) {
         document.getElementById("input_history").setAttribute("style", "display: none;");
@@ -61,6 +70,9 @@ function lives_init() {
     }
 }
 
+/**
+ * Ends the previous round and resets all variables and elements.
+ */
 function end_round() {
     document.getElementById("result").setAttribute("style", "display: none;");
     document.getElementById("fun_check").setAttribute("onclick", "execute();");
@@ -74,6 +86,9 @@ function end_round() {
     futile = 0;
 }
 
+/**
+ * Sets the number of lives.
+ */
 function setLives() {
     for (var i = 1; i <= lives; i++) {
         document.getElementById(`flower${i}`).setAttribute("class", "alive");
@@ -92,6 +107,9 @@ function setLives() {
     }
 }
 
+/**
+ * Clears all textboxes and focuses on the first input.
+ */
 function clear_and_focus() {
     for (var i = 1; i <= data.database[rand].count; i++) {
         document.getElementById(`input${i}`).value = "";
@@ -100,6 +118,9 @@ function clear_and_focus() {
     document.getElementById("input1").focus();
 }
 
+/**
+ * Loads the next question.
+ */
 function qload() {
     end_round();
     /*do {
@@ -143,12 +164,18 @@ function qload() {
     clear_and_focus();
 }
 
+/**
+ * Initial page load.
+ */
 async function load() {
     await fetch("data.json").then(response => response.json()).then(json => data = json);
     lives_init();
     qload();
 }
 
+/**
+ * Reloads the page.
+ */
 function reload() {
     picked = [];
     rand = -1;
@@ -161,6 +188,12 @@ function reload() {
     qload();
 }
 
+/**
+ * Removes type formatting from input values.
+ * This includes double quotes from strings and square brackets from arrays.
+ * @param {Number} n The index of the input being updated
+ * @returns {String} The updated input.
+ */
 function handleInput(n) {
     var args = document.getElementById(`input${n}`).value;
     if (data.database[rand].in[n - 1] == "boolean") {
@@ -183,6 +216,11 @@ function handleInput(n) {
     }
 }
 
+/**
+ * Removes type formatting from output value.
+ * This includes double quotes from strings and square brackets from arrays.
+ * @returns {String} The updated output.
+ */
 function handleOutput() {
     var args = document.getElementById(`output`).value;
     if (data.database[rand].out == "boolean") {
@@ -199,6 +237,12 @@ function handleOutput() {
     }
 }
 
+/**
+ * A function to validate whether the input and output are of the expected types.
+ * @param {Array} input Array of inputs given by the user.
+ * @param {String} output Output given by the user.
+ * @returns {Boolean} true or false based on whether the input and output are valid.
+ */
 function validate(input, output) {
     //Runs a loop to validate every entry in the input array.
     for (var i = 0; i < data.database[rand].count; i++) {
@@ -333,6 +377,12 @@ function validate(input, output) {
     return true;
 }
 
+/**
+ * Function for custom validation of the input, if any.
+ * Pulls the function from the JSON.
+ * @param {Array} input Array of inputs given by the user.
+ * @returns {Boolean} true if the input is valid, false otherwise.
+ */
 function customValidate(input) {
     if (!data.database[rand].customValidate) {
         return true;
@@ -345,6 +395,11 @@ function customValidate(input) {
     return true;
 }
 
+/**
+ * Parses the input values to the correct types and creates an array of them
+ * @param {Array} input The array of inputs as given by the user after removing type formatting
+ * @returns {Array} Parsed input.
+ */
 function updateInputType(input) {
     var arr = [];
     for (var i = 0; i < data.database[rand].count; i++) {
@@ -374,6 +429,11 @@ function updateInputType(input) {
     return arr;
 }
 
+/**
+ * Parses the output value to the correct type.
+ * @param {String} output The output value as given by the user after removing type formatting.
+ * @returns {String} Parsed output.
+ */
 function updateOutputType(output) {
     if (data.database[rand].out == "boolean") {
         var res = (output == "true");
@@ -399,11 +459,21 @@ function updateOutputType(output) {
     return output;
 }
 
+/**
+ * The correct function for the current question.
+ * @param {Array} input The array of inputs to the function.
+ * @returns {data.database[rand].out} The function's output.
+ */
 function correct(input) {
     var f = new Function(...(data.database[rand].correct.arguments), data.database[rand].correct.body);
     return f(...input);
 }
 
+/**
+ * Function that runs all incorrect functions and marks them as killed if there is a discrepancy in the output.
+ * @param {Array} input The array of inputs to the function.
+ * @param {data.database[rand].out} output The correct output. 
+ */
 function incorrect(input, output) {
     var foundInst = 0;
     for (i = 0; i < data.database[rand].numFunc; i++) {
@@ -425,6 +495,12 @@ function incorrect(input, output) {
     }
 }
 
+/**
+ * Used to check equality between user output and expected output.
+ * @param {Object} a1 The first of the values being compared.
+ * @param {Object} a2 The second of the values being compared.
+ * @returns {Boolean} If the values are equal.
+ */
 function resCheck(a1, a2) {
     if (data.database[rand].out == "number") {
         return a1 == a2;
@@ -439,6 +515,11 @@ function resCheck(a1, a2) {
     return true;
 }
 
+/**
+ * Formats the input to the expected pattern.
+ * @param {Array} arr The array of inputs to the function.
+ * @returns {Array} The formatted inputs.
+ */
 function formatInput(arr) {
     res = "";
     if (!arr.length) return arr;
@@ -452,6 +533,9 @@ function formatInput(arr) {
     return res.substring(0, res.length - 1);
 }
 
+/**
+ * Function called whenever the user submits their inputs and outputs.
+ */
 function execute() {
     var args = [];
     for (var i = 1; i <= data.database[rand].count; i++) {
